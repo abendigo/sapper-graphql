6953c22671ad4051a1220d03e20a820fb4092adf
Mark Oosterveld <moosterveld@shuterstock.com>
2019-07-16 15:10:49 -0400

    a little cleanup

--- src/routes/graphiql.js
+++ src/routes/graphiql.js
@@ -1,11 +1,6 @@
 import { graphiqlExpress } from 'apollo-server-express';
 
-const x = graphiqlExpress({
+export const get = graphiqlExpress({
   endpointURL: '/graphql',
   subscriptionsEndpoint: `ws://localhost:3000/subscriptions`
 });
-
-export function get(request, response) {
-  console.log('graphiql:get');
-  return x(request, response);
-}
--- src/server.js
+++ src/server.js
@@ -7,67 +7,36 @@ import http from 'http';
 import express from 'express';
 import { execute, subscribe } from 'graphql';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
-import { makeExecutableSchema } from 'graphql-tools';
 
-import { buildSchema } from 'graphql';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 
-const typeDefs = `
-  type Query {
-    random: Float
-  }
-  type Subscription {
-    randoms: Float
-  }
-`;
-
-const sleep = (milliseconds) => {
-  return new Promise(resolve => setTimeout(resolve, milliseconds))
-}
-const resolvers = {
-  Query: {
-    random: () => Math.random(),
-  },
-  Subscription: {
-    randoms: {
-      subscribe: async function* asyncRandomNumbers() {
-        while (true) {
-          yield { randoms: Math.random() };
-          await sleep(1000);
-        }
-      }
-    }
-  }
-}
-
-const schema = makeExecutableSchema({typeDefs, resolvers});
-
+import { schema } from './_schema';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 const app = express() // You can also use Express
-  .use( bodyParser.json() )
+  // .use( bodyParser.json() )
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
-	)
+	);
 
-  const server = http.createServer(app);
+const server = http.createServer(app);
 
-  server.listen(PORT, () => {
-    console.log("Server started listening on " + PORT);
-    // httpServer.listen({ port: PORT }, () => {
-        // console.log(`server ready at http://localhost:${PORT}${apollo.graphqlPath}`)
-        // console.log(`Subscriptions ready at ws://localhost:${PORT}${apollo.subscriptionsPath}`)
-    //   })
-    new SubscriptionServer({
-      execute,
-      subscribe,
-      schema: schema,
-    }, {
-      server: server,
-      path: '/subscriptions'
-    });
+server.listen(PORT, () => {
+  // console.log("Server started listening on " + PORT);
+  // httpServer.listen({ port: PORT }, () => {
+      // console.log(`server ready at http://localhost:${PORT}${apollo.graphqlPath}`)
+      // console.log(`Subscriptions ready at ws://localhost:${PORT}${apollo.subscriptionsPath}`)
+  //   })
+  const subs = new SubscriptionServer({
+    execute,
+    subscribe,
+    schema: schema,
+  }, {
+    server: server,
+    path: '/subscriptions'
+  });
 });
